d793b87ed36ad737cba01aa7694602259d5ae610
ahyunmoasdiofl1 <mmo0829@naver.com>
2024-12-11 22:34:25 +0900

    Update 2.py

--- 2.py
+++ 2.py
@@ -79,7 +79,7 @@ elif choice == "TIP DOWN":
                 next_state, color = get_next_state(current_state)
 
                 cols[col_idx].markdown(
-                    f"<div style='display: flex; align-items: center; justify-content: space-between;'>"
+                    f"<div style='display: flex; justify-content: space-between;'>"
 
                     f"<div style='width: 20px; height: 20px; background-color: {color}; border-radius: 50%; margin-left: 10px;'></div>"
                     f"</div>",
